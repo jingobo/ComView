0cabb41a96b8b361abc10c5b46362776e571d60b
Vitaly <exclude@bk.ru>
2024-08-27 18:40:36 +0300

    ~ Рефакторинг модуля

--- ComView.Core/Helpers/Limit.cs
+++ ComView.Core/Helpers/Limit.cs
@@ -9,16 +9,23 @@ namespace ComView.Core.Helpers
     /// </summary>
     public struct Limit<T> where T : struct
     {
+        #region static
+        /// <summary>
+        /// Пустое значение
+        /// </summary>
+        public static readonly Limit<T> Empty = new Limit<T>();
+        /// <summary>
+        /// Интерфейс сравнения значений
+        /// </summary>
+        internal static readonly IComparer Comparer = Comparer<T>.Default;
+
+        #endregion
+
         #region fields, ctor
         /// <summary>
         /// Минимум и максимум
         /// </summary>
         public T Min, Max;
-        
-        /// <summary>
-        /// Пустое значение
-        /// </summary>
-        public static readonly Limit<T> Empty = new Limit<T>();
 
         /// <summary>
         /// Конструктор с указанием минимума и максимума
@@ -29,70 +36,64 @@ namespace ComView.Core.Helpers
             Max = max;
         }
         #endregion
+    }
 
-        #region properties
-        /// <summary>
-        /// Получает интерфейс сравнения значений
-        /// </summary>
-        private static IComparer Comparer
-        {
-            get => Comparer<T>.Default;
-        }
-
-        /// <summary>
-        /// Резмер окна предела
-        /// </summary>
-        public T Window
-        {
-            get
-            {
-                dynamic min = Min;
-                dynamic max = Max;
-                return (T)(max - min);
-            }
-        }
-
-        /// <summary>
-        /// Проверка на валидность
-        /// </summary>
-        public bool IsValid
-        {
-            get => Comparer.Compare(Min, Max) <= 0;
-        }
-        #endregion
-
-        #region methods
+    /// <summary>
+    /// Класс расширение для структуры предела
+    /// </summary>
+    public static class LimitHelper
+    {
+        #region validation
         /// <summary>
         /// Проверка на входимость в предел
         /// </summary>
-        public bool IsPassed(T value) =>
-            Comparer.Compare(Min, value) <= 0 && Comparer.Compare(Max, value) >= 0;
+        public static bool IsValid<T>(this Limit<T> limit) where T : struct =>
+            Limit<T>.Comparer.Compare(limit.Min, limit.Max) <= 0;
+
+        /// <summary>
+        /// Производит валидацию предела
+        /// </summary>
+        public static void Validate<T>(this Limit<T> limit) where T : struct
+        {
+            if (limit.IsValid())
+                return;
+
+            throw new InvalidOperationException("limit is invalid");
+        }
+        #endregion
+
+        #region math
+        /// <summary>
+        /// Проверка на входимость в предел
+        /// </summary>
+        public static bool IsPassed<T>(this Limit<T> limit, T value) where T : struct =>
+            Limit<T>.Comparer.Compare(limit.Min, value) <= 0 && 
+            Limit<T>.Comparer.Compare(limit.Max, value) >= 0;
 
         /// <summary>
         /// Нормализация значения
         /// </summary>
-        public T Normalize(T value)
+        public static T Normalize<T>(this Limit<T> limit, T value) where T : struct
         {
-            if (!IsValid)
-                throw new InvalidOperationException("invalid limit");
+            limit.Validate();
 
-            if (Comparer.Compare(Min, value) > 0)
-                return Min;
-            if (Comparer.Compare(Max, value) < 0)
-                return Max;
+            if (Limit<T>.Comparer.Compare(limit.Min, value) > 0)
+                return limit.Min;
+            if (Limit<T>.Comparer.Compare(limit.Max, value) < 0)
+                return limit.Max;
+
             return value;
         }
 
         /// <summary>
         /// Нормализация значения
         /// </summary>
-        public Limit<T> Normalize(Limit<T> limit)
+        public static Limit<T> Normalize<T>(this Limit<T> limit) where T : struct
         {
-            if (!IsValid)
-                throw new InvalidOperationException("invalid limit");
-            
-            limit.Min = Normalize(limit.Min);
-            limit.Max = Normalize(limit.Max);
+            limit.Validate();
+
+            limit.Min = limit.Normalize(limit.Min);
+            limit.Max = limit.Normalize(limit.Max);
             return limit;
         }
         #endregion
